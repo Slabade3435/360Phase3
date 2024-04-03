dfe4f4108506feebc0d1c3ba72069fe47b7468ea
Slabade3435 <sid.sid.labade@gmail.com>
2024-04-02 17:56:21 -0700

    Title change to "this is my branch"

--- projectPhase3/src/application/Main.java
+++ projectPhase3/src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			primaryStage.setTitle("DoomsDay bruh");
+			primaryStage.setTitle("This is my Branch");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
